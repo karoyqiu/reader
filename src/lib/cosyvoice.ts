cd4415bb6c8c2edc0122c8856205f019882d5214
Roy QIU <karoyqiu@gmail.com>
2025-05-29 12:58:21 +0800

    对整个文档调用。

--- src/lib/cosyvoice.ts
+++ src/lib/cosyvoice.ts
@@ -1,17 +1,98 @@
 import { fetch } from '@tauri-apps/plugin-http';
+import { EventEmitter } from 'eventemitter3';
+import PQueue from 'p-queue';
 
 const baseUrl = new URL('http://192.168.0.104:5000');
 const sftUrl = new URL('/inference_sft', baseUrl);
 
-export const textToSpeech = async (text: string, speaker = 'xiaohe') => {
+const textToSpeech = async (signal: AbortSignal, text: string, speaker = 'xiaohe') => {
   const resp = await fetch(sftUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: new URLSearchParams({ tts_text: text, spk_id: speaker }),
+    signal,
   });
   const buffer = await resp.arrayBuffer();
   const int16s = new Int16Array(buffer);
   return new Float32Array([...int16s].map((n) => n / 32768));
 };
+
+type EventName = 'loading' | 'playing' | 'stopped';
+
+export default class CosyVoice extends EventEmitter<EventName> {
+  private readonly queue = new PQueue({ concurrency: 10 });
+  private readonly ctrl = new AbortController();
+  private readonly ctx = new AudioContext();
+  private audioSource: AudioBufferSourceNode | null = null;
+  private data: Float32Array[] = [];
+  private index = 0;
+
+  async speak(lines: string[]) {
+    this.data = new Array<Float32Array>(lines.length);
+
+    await this.queue.addAll(
+      lines.map((line, index) => async ({ signal }) => {
+        console.debug('TTS for line', index);
+        const buffer = await textToSpeech(signal!, line);
+        this.data[index] = buffer;
+        console.debug('TTSed for line', index);
+
+        this.speakFirst();
+      }),
+      { signal: this.ctrl.signal },
+    );
+  }
+
+  async stop() {
+    this.ctrl.abort();
+    this.queue.clear();
+    this.audioSource?.stop();
+    this.audioSource = null;
+  }
+
+  get isPlaying() {
+    return this.data.length > 0;
+  }
+
+  private speakFirst() {
+    if (!this.audioSource) {
+      this.audioSource = this.ctx.createBufferSource();
+      this.speakNext();
+    }
+  }
+
+  private speakNext() {
+    const data = this.data[this.index];
+
+    if (data) {
+      // 有数据，直接播放
+      console.debug('Speaking line', this.index);
+      const buffer = this.ctx.createBuffer(1, data.length, 24000);
+      buffer.copyToChannel(data, 0);
+      this.data[this.index] = new Float32Array(0);
+
+      this.audioSource = this.ctx.createBufferSource();
+      this.audioSource.buffer = buffer;
+      this.audioSource.connect(this.ctx.destination);
+      this.audioSource.addEventListener('ended', () => this.speakNext());
+      this.audioSource.start();
+
+      this.emit('playing', this.index);
+      this.index += 1;
+    } else if (this.index < this.data.length - 1) {
+      // 没数据，也没全部播完
+      console.debug('Waiting for data', this.index);
+      this.audioSource = null;
+      this.emit('loading');
+    } else {
+      // 全部播完了
+      console.debug('All done');
+      this.audioSource = null;
+      this.index = 0;
+      this.data = [];
+      this.emit('stopped');
+    }
+  }
+}
--- src/routes/book.tsx
+++ src/routes/book.tsx
@@ -1,13 +1,13 @@
 import { Link, createFileRoute } from '@tanstack/react-router';
 import { ChevronLeftIcon, ChevronRightIcon, LibraryIcon, SpeechIcon } from 'lucide-react';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { z } from 'zod/v4-mini';
 
 import { BookSidebar } from '@/components/book-sidebar';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { ButtonGroup } from '@/components/ui/button-group';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { textToSpeech } from '@/lib/cosyvoice';
+import CosyVoice from '@/lib/cosyvoice';
 import { getBookContent, getChapterList, saveBookProgress } from '@/lib/legado';
 import { cn } from '@/lib/utils';
 
@@ -17,8 +17,7 @@ const searchSchema = z.object({
   author: z.string(),
   index: z.int(),
 });
-const ctx = new AudioContext();
-let audioSource: AudioBufferSourceNode | null = null;
+const voice = new CosyVoice();
 
 export const Route = createFileRoute('/book')({
   component: Book,
@@ -34,36 +33,16 @@ function Book() {
   const [voiceReady, setVoiceReady] = useState(true);
   const [speaking, setSpeaking] = useState(false);
   const chapter = chapters.find((ch) => ch.index === index);
-  const lines = content.split('\n');
+  const lines = useMemo(() => content.split('\n'), [content]);
 
-  const speak = useCallback(async () => {
-    if (speaking) {
-      audioSource?.stop();
-      audioSource = null;
-      setSpeaking(false);
+  const speak = useCallback(() => {
+    if (voice.isPlaying) {
+      voice.stop();
     } else {
-      try {
-        setVoiceReady(false);
-        const data = await textToSpeech(lines[0]);
-        console.log('float32', data.length);
-        setVoiceReady(true);
-
-        const buffer = ctx.createBuffer(1, data.length, 24000);
-        buffer.copyToChannel(data, 0);
-
-        audioSource = ctx.createBufferSource();
-        audioSource.buffer = buffer;
-        audioSource.connect(ctx.destination);
-        audioSource.addEventListener('ended', () => setSpeaking(false));
-        audioSource.start();
-        setSpeaking(true);
-      } catch (e) {
-        console.error('Failed to speak', e);
-      }
+      setVoiceReady(false);
+      voice.speak(lines).catch(console.error);
     }
-
-    setVoiceReady(true);
-  }, [speaking, content]);
+  }, [lines]);
 
   useEffect(() => {
     saveBookProgress({
@@ -74,6 +53,17 @@ function Book() {
       durChapterTime: Date.now(),
     }).catch(console.error);
   });
+
+  useEffect(() => {
+    voice.addListener('playing', () => {
+      setVoiceReady(true);
+      setSpeaking(true);
+    });
+    voice.addListener('stopped', () => {
+      setVoiceReady(true);
+      setSpeaking(false);
+    });
+  }, []);
 
   return (
     <SidebarProvider defaultOpen={false}>
